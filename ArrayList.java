a9e6eb5bfd8a1279839b937f6ad13309508b1f22
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-03 16:04:03 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,5 @@
+public class ArrayList{
+public static void main(String[]args){
+// prepare an ArrayList Onbject and Add person class Objects of 5 and iterate them using Iterator and While Loop
+}
+}
