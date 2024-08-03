a17a0d81b70307d585eb78966bd5f7e4edf26325
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-03 16:08:42 +0530

    Update ArrayLists.java

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,4 +1,4 @@
-public class ArrayList{
+public class ArrayLists{
 public static void main(String[]args){
 // prepare an ArrayList Onbject and Add person class Objects of 5 and iterate them using Iterator and While Loop
 }
