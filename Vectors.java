44c6d12f1bd8bc362c986efe29d8c10c6a6b599f
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-03 16:07:35 +0530

    Update and rename Vector.java to Vectors.java

--- Vectors.java
+++ Vectors.java
@@ -1,4 +1,4 @@
-public class Vector{
+public class Vectors{
 public static void main(String[]args){
 // prepare a Vector Object and Stack Object add values in them and iterate them using Enumeration and Iterator
 }
