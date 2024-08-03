a92aad9c294d9f700466835698cef6984abcddf2
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-03 16:07:55 +0530

    Update and rename LinkedList.java to LinkedLists.java

--- LinkedLists.java
+++ LinkedLists.java
@@ -1,4 +1,4 @@
-public class LinkedList{
+public class LinkedLists{
 public static void main(String[]args){
 // Preapare a LinkedList Object and Person Class Objects of 5 in it and Using Iterator and While Loop
 }
